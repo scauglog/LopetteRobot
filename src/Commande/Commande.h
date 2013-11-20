96085639c7577962999f376a3e93946cbbc17f63
LASSERRE Christian <classerre@enseirb-matmeca.fr>
2013-11-20 10:32:25 +0100

    API 	Beginning of translation from PROTOCOLE 	to the Command API

--- src/Commande/Commande.h
+++ src/Commande/Commande.h
@@ -1,17 +1,37 @@
-getProtocolVersion
-reset
-failSafe
-setMode
-setHeartBeat
-heartBeat
-setState
-getState
-read
-write
-monitorRead
+/**
+ * 
+ **/
+/* Arduino Receptionning commands */
 
-error
-ok
-protocolVersion
-statesValues
-pinsValues
+/**
+ *@param minSupported minimal version supported by the Raspberry
+ *@param maxSupported minimal version supported by the Raspberry
+ *@return version in which Raspberry and Arduino will communicate
+ **/
+char getProtocolVersion(char minSupported, char maxSupported);
+/**
+ *@param type of reset (see definition in PROTOCOLE)
+ *@return ok (see definition in PROTOCOLE)
+ **/
+char reset(char type);
+/**
+ *@param
+ **/
+int failSafe(char states[6], char *values);
+
+int setMode();
+int setHeartBeat();
+int heartBeat();
+int setState();
+int getState();
+int read();
+int write();
+int monitorRead();
+
+
+/* Arduino Answering commands */
+int error();
+int ok();
+int protocolVersion();
+int statesValues();
+int pinsValues();

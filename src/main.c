8cf51ef0d9ba6891d7a542f51bcb8039ebc0e9e5
scauglog <scauglog@gmail.com>
2013-11-27 16:14:06 +0100

    BOUCLE HELLO WOLRD 	send hello world every second through serial port

--- src/main.c
+++ src/main.c
@@ -6,14 +6,14 @@
 
 void myputchar(char i){
   UDR=i;
-  while(!(USR&(1<<TXC)));
+  while(!(UCSRA&(1<<UDRE)));
 }
 
 ISR(UART_RXC)
 {
   char a;
   a=UDR;
-  if( a=='o'){
+  if( 1){
     PORTB = 0b00100000;
   }
   else
@@ -23,9 +23,13 @@ ISR(UART_RXC)
 
 int main(){                          // The main function
   DDRB = 0b11111111;// Set all the pins of PORTB as output
-  UCR=(1<<RXCIE)|(1<<RXEN)|(1<<TXEN);
+  UCSRB=(1<<RXCIE)|(1<<RXEN)|(1<<TXEN);
   sei();
   
+while (1) {                        // Set up an infinite loop                   
+  
+  PORTB = 0b11111111;
+    
   myputchar('H');
   myputchar('E');
   myputchar('L');
@@ -36,13 +40,10 @@ int main(){                          // The main function
   myputchar('O');
   myputchar('R');
   myputchar('L');
-  myputchar('D');
-
-while (1) {                        // Set up an infinite loop                   
-  //PORTB = 0b00000001;
-  //  _delay_ms(50);
-    
-    
+  myputchar('D'); 
+  _delay_ms(100); 
+  PORTB = 0b00000000; 
+  _delay_ms(1000);  
     
   }
 }
